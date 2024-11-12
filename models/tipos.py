a262f193a38546687c01e7e9ccc8d7c029a26e35
cesaraugustooo <164028550+cesaraugustooo@users.noreply.github.com>
2024-11-12 07:32:57 -0300

    Delete models directory

--- models/tipos.py
+++
@@ -1,9 +0,0 @@
-from peewee import AutoField,CharField,Model
-from config.database import database
-
-class TiposDB(Model):
-    id = AutoField()
-    nome_tipo = CharField()
-
-    class Meta:
-        database = database
